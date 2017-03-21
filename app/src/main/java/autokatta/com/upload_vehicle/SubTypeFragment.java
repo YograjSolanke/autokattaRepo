ad2501c6d19b16a95c461bf4044d650b77a28311
ak-001 <dpkpanchal38@gmail.com>
2017-03-21 18:58:45 +0530

    21/03/2017 vehicle upload

--- app/src/main/java/autokatta/com/upload_vehicle/SubTypeFragment.java
+++ app/src/main/java/autokatta/com/upload_vehicle/SubTypeFragment.java
@@ -1,8 +1,10 @@
 package autokatta.com.upload_vehicle;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -31,19 +33,88 @@ public class SubTypeFragment extends Fragment {
         mRegisterMonth = (EditText) mSubtype.findViewById(R.id.register_month);
         mRegisterYear = (EditText) mSubtype.findViewById(R.id.register_year);
 
+        mMakeMonth.setInputType(InputType.TYPE_NULL);
+        mMakeYear.setInputType(InputType.TYPE_NULL);
+        mRegisterMonth.setInputType(InputType.TYPE_NULL);
+        mRegisterYear.setInputType(InputType.TYPE_NULL);
+
+        mMakeMonth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myp.show();
+                makeMonth();
+            }
+        });
+
+        mMakeYear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myp.show();
+                myp.build(new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mMakeMonth.setText(myp.getSelectedMonthName());
+                        mMakeYear.setText(myp.getSelectedYear());
+                    }
+                }, null);
+            }
+        });
+
+        mRegisterMonth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myp.show();
+                myp.build(new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mRegisterMonth.setText(myp.getSelectedMonthName());
+                        mRegisterYear.setText(myp.getSelectedYear());
+                    }
+                }, null);
+            }
+        });
+
+        mRegisterYear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myp.show();
+                myp.build(new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mRegisterMonth.setText(myp.getSelectedMonthName());
+                        mRegisterYear.setText(myp.getSelectedYear());
+                    }
+                }, null);
+            }
+        });
         /*mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 myp.show();
             }
-        });
-        myp = new MonthYearPicker(getActivity());
-        myp.build(new OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                textView1.setText(myp.getSelectedMonthName() + " >> " + myp.getSelectedYear());
-            }
-        }, null);*/
+        });*/
+
         return mSubtype;
     }
+
+    private void makeMonth() {
+        myp = new MonthYearPicker(getActivity());
+        myp.build(new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                mMakeMonth.setText(myp.getSelectedMonthName());
+                mMakeYear.setText(myp.getSelectedYear());
+            }
+        }, null);
+    }
 }
+
+    /*@Override
+    public boolean onTouch(View v, MotionEvent event) {
+        switch (v.getId()) {
+            case R.id.make_month:
+                myp.show();
+                break;
+        }
+        return false;
+    }*/
