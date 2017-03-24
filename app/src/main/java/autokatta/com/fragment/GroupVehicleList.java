79e955fcad0f15bba546af6431feb11f49d49ee3
ak-001 <dpkpanchal38@gmail.com>
2017-03-24 20:06:16 +0530

    24/03/2017 card view implemented...

--- app/src/main/java/autokatta/com/fragment/GroupVehicleList.java
+++ app/src/main/java/autokatta/com/fragment/GroupVehicleList.java
@@ -13,12 +13,14 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import autokatta.com.R;
 import autokatta.com.apicall.ApiCall;
 import autokatta.com.interfaces.RequestNotifier;
 import autokatta.com.other.CustomToast;
+import autokatta.com.response.GetGroupVehiclesResponse;
 import retrofit2.Response;
 
 /**
@@ -34,6 +36,7 @@ public class GroupVehicleList extends Fragment implements RequestNotifier {
     AutoCompleteTextView editRTOcity;
     ImageView filterimg;
     RelativeLayout relativefilter;
+    List<GetGroupVehiclesResponse.Success> mSuccesses = new ArrayList<>();
 
     @Nullable
     @Override
@@ -103,7 +106,53 @@ public class GroupVehicleList extends Fragment implements RequestNotifier {
     public void notifySuccess(Response<?> response) {
         if (response!=null){
             if (response.isSuccessful()){
-
+                if (response.body() instanceof GetGroupVehiclesResponse){
+                    GetGroupVehiclesResponse mGetGroupVehiclesResponse = (GetGroupVehiclesResponse) response.body();
+                    for (GetGroupVehiclesResponse.Success success : mGetGroupVehiclesResponse.getSuccess()){
+                        success.setVehicleId(success.getVehicleId());
+                        success.setTitle(success.getTitle());
+                        success.setCategory(success.getCategory());
+                        success.setModel(success.getModel());
+                        success.setManufacturer(success.getManufacturer());
+                        success.setRTOCity(success.getRTOCity());
+                        success.setLocationCity(success.getLocationCity());
+                        success.setLocationState(success.getLocationState());
+                        success.setLocationCountry(success.getLocationCountry());
+                        success.setLocationAddress(success.getLocationAddress());
+                        success.setYearOfRegistration(success.getYearOfRegistration());
+                        success.setYearOfManufacture(success.getYearOfManufacture());
+                        success.setContact(success.getContact());
+                        success.setColor(success.getColor());
+                        success.setRegistrationNumber(success.getRegistrationNumber());
+                        success.setRcAvailable(success.getRcAvailable());
+                        success.setInsuranceValid(success.getInsuranceValid());
+                        success.setInsuranceIdv(success.getInsuranceIdv());
+                        success.setTaxValidity(success.getTaxValidity());
+                        success.setFitnessValidity(success.getFitnessValidity());
+                        success.setPermitValidity(success.getPermitValidity());
+                        success.setFualType(success.getFualType());
+                        success.setSeatingCapacity(success.getSeatingCapacity());
+                        success.setPermit(success.getPermit());
+                        success.setKmsRunning(success.getKmsRunning());
+                        success.setNoOfOwners(success.getNoOfOwners());
+                        success.setHypothication(success.getHypothication());
+                        success.setEngineNo(success.getEngineNo());
+                        success.setChassisNo(success.getChassisNo());
+                        success.setPrice(success.getPrice());
+                        success.setImage(success.getImage());
+                        success.setDrive(success.getDrive());
+                        success.setTransmission(success.getTransmission());
+                        success.setBodyType(success.getBodyType());
+                        success.setBoatType(success.getBoatType());
+                        success.setRvType(success.getRvType());
+                        success.setApplication(success.getApplication());
+                        success.setStatus(success.getStatus());
+                        success.setDate(success.getDate());
+                        success.setVehiclelikestatus(success.getVehiclelikestatus());
+                        success.setUsername(success.getUsername());
+                        mSuccesses.add(success);
+                    }
+                }
             }else {
                 CustomToast.customToast(getActivity(),getString(R.string._404));
             }
