b6962006e92831f3d542194b35b2611ac4a168c9
rajsanket <varude.rajashree@gmail.com>
2017-04-04 10:44:45 +0530

    4-04-17

--- app/src/main/java/autokatta/com/adapter/MySearchAdapter.java
+++ app/src/main/java/autokatta/com/adapter/MySearchAdapter.java
@@ -1,8 +1,52 @@
 package autokatta.com.adapter;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import autokatta.com.interfaces.RequestNotifier;
+import retrofit2.Response;
+
 /**
  * Created by ak-004 on 3/4/17.
  */
 
-public class MySearchAdapter {
+public class MySearchAdapter extends RecyclerView.Adapter<MySearchAdapter.SearchHolder> implements RequestNotifier {
+    @Override
+    public MySearchAdapter.SearchHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(MySearchAdapter.SearchHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+
+    public static class SearchHolder extends RecyclerView.ViewHolder {
+
+        public SearchHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+    @Override
+    public void notifySuccess(Response<?> response) {
+
+    }
+
+    @Override
+    public void notifyError(Throwable error) {
+
+    }
+
+    @Override
+    public void notifyString(String str) {
+
+    }
 }
