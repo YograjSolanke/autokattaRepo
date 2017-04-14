9a6ff885098d9a0f932fb6333cdf2cdd911a5d03
ak-001 <dpkpanchal38@gmail.com>
2017-04-14 19:13:06 +0530

    14/04/2017 my Auction Vehicle Details...

---
+++ app/src/main/java/autokatta/com/auction/MyAuctionVehicleDetails.java
@@ -0,0 +1,15 @@
+package autokatta.com.auction;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import autokatta.com.R;
+
+public class MyAuctionVehicleDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_auction_vehicle_details);
+    }
+}
