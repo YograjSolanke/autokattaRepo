d95dafed5bd88fcf9cda61aa4c3b818c89305a6c
Yograj004 <yograj004@gmail.com>
2017-05-05 18:21:57 +0530

    05-05-2017 Add manual enquiry

--- app/src/main/java/autokatta/com/view/AddManualEnquiry.java
+++ app/src/main/java/autokatta/com/view/AddManualEnquiry.java
@@ -5,7 +5,9 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
 import android.text.InputType;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,15 +16,19 @@ import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.net.SocketTimeoutException;
+import java.util.List;
 
 import autokatta.com.R;
 import autokatta.com.adapter.GooglePlacesAdapter;
 import autokatta.com.apicall.ApiCall;
 import autokatta.com.generic.SetMyDateAndTime;
 import autokatta.com.interfaces.RequestNotifier;
+import autokatta.com.other.CustomToast;
+import autokatta.com.response.AddManualEnquiryResponse;
 import retrofit2.Response;
 
 public class AddManualEnquiry extends AppCompatActivity implements RequestNotifier, View.OnTouchListener {
@@ -31,6 +37,8 @@ public class AddManualEnquiry extends AppCompatActivity implements RequestNotifi
     AutoCompleteTextView autoAddress;
     Spinner spnInventory, spnStatus;
     String myContact;
+    TextView txtUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,7 +48,7 @@ public class AddManualEnquiry extends AppCompatActivity implements RequestNotifi
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
-        myContact = getSharedPreferences(getString(R.string.my_preference), MODE_PRIVATE).getString("loginContact", "7841023392");
+        myContact = getSharedPreferences(getString(R.string.my_preference), MODE_PRIVATE).getString("loginContact", "");
         edtName = (EditText) findViewById(R.id.edtName);
         edtContact = (EditText) findViewById(R.id.edtContact);
         edtAddress = (EditText) findViewById(R.id.edtAddress);
@@ -50,10 +58,58 @@ public class AddManualEnquiry extends AppCompatActivity implements RequestNotifi
         spnStatus = (Spinner) findViewById(R.id.spnStatus);
         edtDate = (EditText) findViewById(R.id.edtDate);
         edtTime = (EditText) findViewById(R.id.edtTime);
+        txtUser = (TextView) findViewById(R.id.txtUser);
 
         edtDate.setOnTouchListener(this);
         edtTime.setOnTouchListener(this);
         autoAddress.setAdapter(new GooglePlacesAdapter(AddManualEnquiry.this, R.layout.registration_spinner));
+
+        edtContact.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                txtUser.setVisibility(View.GONE);
+                if (s.length() == 10) {
+                    if (!myContact.equalsIgnoreCase(s.toString()))
+                        checkUser(s.toString());
+                    else {
+                        edtContact.requestFocus();
+                        edtContact.setError("admin not allowed");
+                    }
+                }
+
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                txtUser.setVisibility(View.GONE);
+            }
+        });
+
+        txtUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ActivityOptions options = ActivityOptions.makeCustomAnimation(AddManualEnquiry.this, R.anim.left_to_right, R.anim.right_to_left);
+                Bundle bundle = new Bundle();
+                bundle.putString("contactOtherProfile", edtContact.getText().toString());
+                Intent intent = new Intent(AddManualEnquiry.this, OtherProfile.class);
+                intent.putExtras(bundle);
+                startActivity(intent, options.toBundle());
+            }
+        });
+    }
+
+    private void checkUser(String contact) {
+
+        ApiCall mApiCall = new ApiCall(this, this);
+        mApiCall.registrationContactValidation(contact);
     }
 
     @Override
@@ -75,7 +131,10 @@ public class AddManualEnquiry extends AppCompatActivity implements RequestNotifi
                 break;
 
             case R.id.ok_manual:
+
                 String custInventoryType = "", custEnquiryStatus = "";
+                Boolean flag = false;
+
                 int strPos = spnInventory.getSelectedItemPosition();
                 int strPos1 = spnStatus.getSelectedItemPosition();
 
@@ -94,10 +153,53 @@ public class AddManualEnquiry extends AppCompatActivity implements RequestNotifi
                 String discussion = edtDiscussion.getText().toString();
                 String nextFollowupDate = edtDate.getText().toString() + " " + edtTime.getText().toString();
 
-                AddEnquiryData(custName, custContact, custAddress, custFullAddress, custInventoryType, custEnquiryStatus, discussion, nextFollowupDate);
-                ActivityOptions options = ActivityOptions.makeCustomAnimation(AddManualEnquiry.this, R.anim.ok_left_to_right, R.anim.ok_right_to_left);
-                startActivity(new Intent(getApplicationContext(), ManualEnquiry.class), options.toBundle());
-                finish();
+                if (!custAddress.isEmpty()) {
+
+                    List<String> resultList = GooglePlacesAdapter.getResultList();
+                    for (int i = 0; i < resultList.size(); i++) {
+
+                        if (custAddress.equalsIgnoreCase(resultList.get(i))) {
+                            flag = true;
+                            break;
+
+                        } else {
+
+                            flag = false;
+                        }
+                    }
+                }
+
+                if (custName.equalsIgnoreCase("")) {
+                    edtName.setError("Please provide customer name");
+                    edtName.requestFocus();
+                } else if (custContact.isEmpty()) {
+                    edtContact.setError("Please provide customer contact");
+                    edtContact.requestFocus();
+                } else if (custAddress.equals("")) {
+                    autoAddress.setError("Enter Address");
+                    autoAddress.requestFocus();
+                } else if (!flag) {
+                    autoAddress.setError("Please provide proper address");
+                    autoAddress.requestFocus();
+                } else if (custFullAddress.equals("")) {
+                    edtAddress.setError("Enter detailed address");
+                    edtAddress.requestFocus();
+                } else if (spnInventory.getSelectedItemPosition() == 0) {
+                    Toast.makeText(getApplicationContext(), "Please provide inventory", Toast.LENGTH_SHORT).show();
+                    spnInventory.requestFocus();
+                } else if (spnStatus.getSelectedItemPosition() == 0) {
+                    Toast.makeText(getApplicationContext(), "Please provide status", Toast.LENGTH_SHORT).show();
+                    spnStatus.requestFocus();
+                } else if (discussion.equals("")) {
+                    edtDiscussion.setError("Enter discussion data");
+                    edtDiscussion.requestFocus();
+                } else if (nextFollowupDate.equals("") || nextFollowupDate.startsWith(" ")) {
+                    edtDate.setError("Enter Date");
+                    edtDate.requestFocus();
+                } else
+                    AddEnquiryData(custName, custContact, custAddress, custFullAddress, custInventoryType, custEnquiryStatus,
+                            discussion, nextFollowupDate);
+
                 return true;
         }
 
@@ -155,7 +257,17 @@ public class AddManualEnquiry extends AppCompatActivity implements RequestNotifi
 
     @Override
     public void notifySuccess(Response<?> response) {
+        AddManualEnquiryResponse enquiryResponse = (AddManualEnquiryResponse) response.body();
 
+        if (enquiryResponse.getSuccess() != null) {
+            if (enquiryResponse.getSuccess().getMessage().equalsIgnoreCase("Data successfully Inserted.")) {
+                Toast.makeText(getApplicationContext(), enquiryResponse.getSuccess().getMessage(), Toast.LENGTH_SHORT).show();
+                ActivityOptions options = ActivityOptions.makeCustomAnimation(AddManualEnquiry.this, R.anim.ok_left_to_right, R.anim.ok_right_to_left);
+                startActivity(new Intent(getApplicationContext(), ManualEnquiry.class), options.toBundle());
+                finish();
+            }
+        } else
+            Toast.makeText(getApplicationContext(), getString(R.string.no_internet), Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -175,6 +287,11 @@ public class AddManualEnquiry extends AppCompatActivity implements RequestNotifi
 
     @Override
     public void notifyString(String str) {
-
+        if (str != null) {
+            if (str.equalsIgnoreCase("Success")) {
+                txtUser.setVisibility(View.VISIBLE);
+            }
+        } else
+            CustomToast.customToast(getApplicationContext(), getString(R.string.no_response));
     }
 }
