79f7625448b3cf8e763c6b9955251ac794121305
ak-005 <rutunaukudkar@gmail.com>
2017-07-11 14:46:49 +0530

    11/07/2017 registration

--- app/src/main/java/autokatta/com/register/Registration.java
+++ app/src/main/java/autokatta/com/register/Registration.java
@@ -28,6 +28,7 @@ import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.net.ConnectException;
 import java.net.SocketTimeoutException;
@@ -58,7 +59,7 @@ public class Registration extends AppCompatActivity implements View.OnClickListe
     TextInputLayout otherIndustryLayout, otherCategoryLayout;
     ImageView clearDate;
     String namestr, contactstr, emailstr, DOBstr, pincodestr, passwordstr, confirmpassstr, addressstr, genderstr,
-            profession, sub_profession, strIndustry;
+            profession="", sub_profession="", strIndustry="";
 
     RelativeLayout mRegistration;
     LinearLayout mLinear, mButton;
@@ -175,8 +176,14 @@ public class Registration extends AppCompatActivity implements View.OnClickListe
                 genderstr = ((RadioButton) findViewById(rg1.getCheckedRadioButtonId())).getText().toString().trim();
 
                 profession = usertypeSpinner.getSelectedItem().toString().trim();
-                strIndustry = industrySpinner.getSelectedItem().toString().trim();
-                sub_profession = moduleSpinner.getSelectedItem().toString().trim();
+
+                if (!profession.equalsIgnoreCase("Student")) {
+                    strIndustry = industrySpinner.getSelectedItem().toString().trim();
+
+                }
+                if (moduleSpinner.getVisibility()==View.VISIBLE)
+                    sub_profession = moduleSpinner.getSelectedItem().toString().trim();
+
 
                 if (!addressstr.isEmpty()) {
                     resultList = GooglePlacesAdapter.getResultList();
@@ -233,7 +240,44 @@ public class Registration extends AppCompatActivity implements View.OnClickListe
                 } else if (!passwordstr.equals(confirmpassstr)) {
                     confirmPassword.setError("Confirm password is wrong");
                     confirmPassword.requestFocus();
+                } else if (profession.equalsIgnoreCase("Select User Type")) {
+                    Toast.makeText(getApplicationContext(), "Please select User type", Toast.LENGTH_LONG).show();
+                } else if ((!profession.equalsIgnoreCase("Student") && (strIndustry.equalsIgnoreCase("")||strIndustry.equalsIgnoreCase("Select Industry")))) {
+                    Toast.makeText(Registration.this, "Please select industry", Toast.LENGTH_LONG).show();
+                } else if (strIndustry.equalsIgnoreCase("Other") && otherIndustry.getText().toString().trim().equalsIgnoreCase("")) {
+                    otherIndustry.setError("Enter Industry");
+                } else if (strIndustry.equalsIgnoreCase("Other") && !otherIndustry.getText().toString().matches("[a-zA-Z ]*")) {
+                    otherIndustry.setError("Enter  Valid Industry");
+                }// else if (strIndustry.equalsIgnoreCase("Automotive and vehicle manufacturing") && sub_profession.equalsIgnoreCase("Select Category")) {
+                else if (strIndustry.equalsIgnoreCase("Automotive") && sub_profession.equalsIgnoreCase("Select Category")) {
+                    Toast.makeText(Registration.this, "Please select category", Toast.LENGTH_LONG).show();
+                } else if (sub_profession.equalsIgnoreCase("other") && otherCategory.getText().toString().equalsIgnoreCase("")) {
+                    otherCategory.setError("Enter Profession");
+                } else if (sub_profession.equalsIgnoreCase("other") && !otherCategory.getText().toString().matches("[a-zA-Z ]*")) {
+                    otherCategory.setError("Enter  Valid Profession");
+                } else if (passwordstr.isEmpty()) {
+                    password.setError("Enter Password");
+                } else if (passwordstr.length() < 6) {
+                    password.setError("Password should be minimum 6 characters");
+                    password.requestFocus();
+                } else if (confirmpassstr.isEmpty()) {
+                    confirmPassword.setError("Enter Password to confirm");
+                    confirmPassword.requestFocus();
+                } else if (!passwordstr.equals(confirmpassstr)) {
+                    confirmPassword.setError("Confirm password is wrong");
+                    confirmPassword.requestFocus();
+                } else if (sub_profession.equalsIgnoreCase("Other")) {
+                    sub_profession = otherCategory.getText().toString().trim();
+                    apiCall.addOtherCategory(sub_profession);
+                } else if (strIndustry.equalsIgnoreCase("Other")) {
+                    strIndustry = otherIndustry.getText().toString().trim();
+                    apiCall.addOtherIndustry(strIndustry);
+                } else if (sub_profession.equalsIgnoreCase("Select Category")) {
+                    sub_profession = "";
+                } else if (strIndustry.equalsIgnoreCase("Select Industry")) {
+                    strIndustry = "";
                 } else {
+
                     apiCall.registrationAfterOtp(namestr, contactstr, emailstr, DOBstr, genderstr, pincodestr, addressstr, profession, passwordstr, sub_profession, strIndustry);
                 }
 
@@ -329,7 +373,7 @@ public class Registration extends AppCompatActivity implements View.OnClickListe
                 break;
             case (R.id.spinnerindustry):
                 // strIndustry = industrySpinner.getSelectedItem().toString();
-                if (industrySpinner.getSelectedItem().toString().equalsIgnoreCase("Automotive and vehicle manufacturing")) {
+                if (industrySpinner.getSelectedItem().toString().equalsIgnoreCase("Automotive")) {
                     // otherCmoduleSpinner = (Spinner) findViewById(R.id.spinner_cou);ategorylayout.setVisibility(View.VISIBLE);
                     moduleSpinner.setVisibility(View.VISIBLE);
                     otherIndustryLayout.setVisibility(View.GONE);
