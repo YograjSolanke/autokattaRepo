90ef509e8331a577c3690b75af6a9891cae9ac93
rajsanket <varude.rajashree@gmail.com>
2017-09-27 19:39:34 +0530

    review implemented code commit(27-09-17)

--- app/src/main/java/autokatta/com/view/ReviewActivity.java
+++ app/src/main/java/autokatta/com/view/ReviewActivity.java
@@ -1,6 +1,8 @@
 package autokatta.com.view;
 
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
@@ -15,7 +17,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,8 +29,10 @@ import java.net.SocketTimeoutException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
+import java.util.TimeZone;
 
 import autokatta.com.R;
 import autokatta.com.adapter.ReviewAdapter;
@@ -54,6 +60,10 @@ public class ReviewActivity extends AppCompatActivity implements RequestNotifier
     int store_id, product_id, service_id, vehicle_id;
     ImageView uploadImage;
     TextView addimagetext;
+    private LinearLayout mLinearListView;
+    boolean isFirstViewClick[];
+    LinearLayout mLinearScrollSecond[];
+    RelativeLayout relativeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,7 +82,9 @@ public class ReviewActivity extends AppCompatActivity implements RequestNotifier
         dialog.setMessage("Loading...");
         mConnectionDetector = new ConnectionDetector(this);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        listView = (ListView) findViewById(R.id.msgview);
+        // listView = (ListView) findViewById(R.id.msgview);
+        mLinearListView = (LinearLayout) findViewById(R.id.linear_ListView);
+
         fab.setOnClickListener(this);
 
         runOnUiThread(new Runnable() {
@@ -120,7 +132,7 @@ public class ReviewActivity extends AppCompatActivity implements RequestNotifier
             case R.id.fab:
                 hud.dismiss();
                 //write review here
-                sendmessage();
+                sendmessage(0, "Review");
                 break;
 
         }
@@ -129,7 +141,7 @@ public class ReviewActivity extends AppCompatActivity implements RequestNotifier
 
     //alert dialog box method to show pop up to send message and image in broadcast group
 
-    public void sendmessage() {
+    public void sendmessage(final int review_id, final String keyword) {
         final AlertDialog.Builder alertDialog = new AlertDialog.Builder(ReviewActivity.this);
         LayoutInflater inflater = getLayoutInflater();
         View convertView = inflater.inflate(R.layout.custom_broadmessage_layout, null);
@@ -165,7 +177,7 @@ public class ReviewActivity extends AppCompatActivity implements RequestNotifier
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mApiCall.postReviewOrReply(0, "Review", contact, message.getText().toString(), store_id, product_id, service_id, vehicle_id);
+                mApiCall.postReviewOrReply(review_id, keyword, contact, message.getText().toString(), store_id, product_id, service_id, vehicle_id);
                 alert.dismiss();
             }
         });
@@ -176,6 +188,19 @@ public class ReviewActivity extends AppCompatActivity implements RequestNotifier
 
             }
         });
+    }
+
+    public void setViewsVisible(int s) {
+        for (int i = 0; i < mainList.size(); i++) {
+            if (s == i && !isFirstViewClick[i]) {
+                isFirstViewClick[i] = true;
+                mLinearScrollSecond[i].setVisibility(View.VISIBLE);
+            } else {
+                isFirstViewClick[i] = false;
+                mLinearScrollSecond[i].setVisibility(View.GONE);
+            }
+
+        }
     }
 
 
@@ -191,32 +216,145 @@ public class ReviewActivity extends AppCompatActivity implements RequestNotifier
                  */
                 if (response.body() instanceof ReviewAndReplyResponse) {
                     ReviewAndReplyResponse moduleResponse = (ReviewAndReplyResponse) response.body();
-                    childlist = new ArrayList<>();
+                    mainList.clear();
+//                    childlist.clear();
                     ReviewAndReplyResponse.Success object = moduleResponse.getSuccess();
                     for (ReviewAndReplyResponse.Success.ReviewMessage obj : object.getReviewMessage()) {
+                        childlist = new ArrayList<>();
                         obj.setSenderContact(obj.getSenderContact());
                         obj.setReviewString(obj.getReviewString());
                         obj.setCreatedDate(obj.getCreatedDate());
                         obj.setReviewId(obj.getReviewId());
+                        obj.setUsername(obj.getUsername());
+
+
+                        for (ReviewAndReplyResponse.Success.ReplayMessage objectmatch : object.getReplayMessage()) {
+                            if (obj.getReviewId().equals(objectmatch.getReviewId())) {
+                                objectmatch.setCreatedDate(objectmatch.getCreatedDate());
+                                objectmatch.setReplayId(objectmatch.getReplayId());
+                                objectmatch.setSenderContact(objectmatch.getSenderContact());
+                                objectmatch.setReplayString(objectmatch.getReplayString());
+                                childlist.add(objectmatch);
+                            }
+
+                        }
 
                         obj.setReplayMessage(childlist);
                         mainList.add(obj);
-
                     }
 
-                    for (ReviewAndReplyResponse.Success.ReplayMessage objectmatch : object.getReplayMessage()) {
-                        objectmatch.setCreatedDate(objectmatch.getCreatedDate());
-                        objectmatch.setReplayId(objectmatch.getReplayId());
-                        objectmatch.setSenderContact(objectmatch.getSenderContact());
-                        objectmatch.setReplayString(objectmatch.getReplayString());
-                        childlist.add(objectmatch);
+                    //here is code??????????????????????????????????????????????????
 
+                    System.out.println("main list size=" + mainList.size());
+                    mLinearScrollSecond = new LinearLayout[mainList.size()];
+                    // isFirstViewClick = new boolean[mainList.size()];
+
+                    //Adds data into first row
+                    for (int i = 0; i < mainList.size(); i++) {
+                        LayoutInflater inflater = null;
+                        // int mFlippingsell = 0;
+                        inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                        View mLinearView = inflater.inflate(R.layout.review_layout, null);
+                        final TextView msg = (TextView) mLinearView.findViewById(R.id.msgr);
+                        final TextView dateNtime = (TextView) mLinearView.findViewById(R.id.dateNtime);
+                        final RelativeLayout mLinearFirstArrow = (RelativeLayout) mLinearView.findViewById(R.id.linearFirst);
+                        final ImageView replyImage = (ImageView) mLinearView.findViewById(R.id.reply);
+                        mLinearScrollSecond[i] = (LinearLayout) mLinearView.findViewById(R.id.linear_scroll);
+//
+//                        //checkes if menu is already opened or not
+//                        if (!isFirstViewClick[i]) {
+//                            mLinearScrollSecond[i].setVisibility(View.GONE);
+//                            //mImageArrowFirst.setBackgroundResource(R.drawable.arw_lt);
+//                        } else {
+//                            mLinearScrollSecond[i].setVisibility(View.VISIBLE);
+//                            //mImageArrowFirst.setBackgroundResource(R.drawable.arw_down);
+//                        }
+
+//                        final int villll = i;
+//                        mLinearFirstArrow.setOnClickListener(new View.OnClickListener() {
+//                            @Override
+//                            public void onClick(View v) {
+//                                setViewsVisible(villll);
+//                            }
+//                        });
+
+                        msg.setText(mainList.get(i).getReviewString());
+
+                        try {
+                            TimeZone utc = TimeZone.getTimeZone("etc/UTC");
+                            //format of date coming from services
+                            DateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+                        /*DateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss",
+                                Locale.getDefault());*/
+                            inputFormat.setTimeZone(utc);
+
+                            //format of date which we want to show
+                            DateFormat outputFormat = new SimpleDateFormat("dd MMM yyyy", Locale.getDefault());
+                        /*DateFormat outputFormat = new SimpleDateFormat("dd MMM yyyy hh:mm aa",
+                                Locale.getDefault());*/
+                            outputFormat.setTimeZone(utc);
+
+                            Date date = inputFormat.parse(mainList.get(i).getCreatedDate());
+                            //System.out.println("jjj"+date);
+                            String output = outputFormat.format(date);
+                            //System.out.println(mainList.get(i).getDate()+" jjj " + output);
+                            dateNtime.setText(mainList.get(i).getUsername() + " " + output);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+                        final int finalI = i;
+                        replyImage.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                sendmessage(mainList.get(finalI).getReviewId(), "Reply");
+                            }
+                        });
+
+
+                        //Adds data into second row
+                        for (int j = 0; j < mainList.get(i).getReplayMessage().size(); j++) {
+                            LayoutInflater inflater2 = null;
+                            int showcheckboc = 0;
+                            inflater2 = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                            View mLinearView2 = inflater2.inflate(R.layout.reply_layout, null);
+
+                            TextView reply = (TextView) mLinearView2.findViewById(R.id.msgr);
+
+                            TextView repdateNtime = (TextView) mLinearView2.findViewById(R.id.dateNtime);
+                            reply.setText(mainList.get(i).getReplayMessage().get(j).getReplayString());
+
+
+                            try {
+                                TimeZone utc = TimeZone.getTimeZone("etc/UTC");
+                                //format of date coming from services
+                                DateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+                        /*DateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss",
+                                Locale.getDefault());*/
+                                inputFormat.setTimeZone(utc);
+
+                                //format of date which we want to show
+                                DateFormat outputFormat = new SimpleDateFormat("dd MMM yyyy", Locale.getDefault());
+                        /*DateFormat outputFormat = new SimpleDateFormat("dd MMM yyyy hh:mm aa",
+                                Locale.getDefault());*/
+                                outputFormat.setTimeZone(utc);
+
+                                Date date = inputFormat.parse(mainList.get(i).getReplayMessage().get(j).getCreatedDate());
+                                //System.out.println("jjj"+date);
+                                String output = outputFormat.format(date);
+                                //System.out.println(mainList.get(i).getDate()+" jjj " + output);
+                                repdateNtime.setText(output);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+
+                            mLinearScrollSecond[i].addView(mLinearView2);
+                        }
+                        mLinearListView.addView(mLinearView);
                     }
 
 
-                    adapter = new ReviewAdapter(ReviewActivity.this, mainList, contact);
-                    listView.setAdapter(adapter);
-                    adapter.notifyDataSetChanged();
+                    //????????????????????????????????????????????????????????????
 
                 } else {
                     hud.dismiss();
@@ -251,12 +389,20 @@ public class ReviewActivity extends AppCompatActivity implements RequestNotifier
             if (str.equals("sent_review")) {
                 hud.dismiss();
                 CustomToast.customToast(ReviewActivity.this, "Review Posted");
-                mApiCall.getReviewOrReply(store_id, product_id, service_id, vehicle_id);
+                //   mApiCall.getReviewOrReply(store_id, product_id, service_id, vehicle_id);
+
             } else if (str.equals("sent_reply")) {
                 hud.dismiss();
                 CustomToast.customToast(ReviewActivity.this, "Reply Sent");
                 mApiCall.getReviewOrReply(store_id, product_id, service_id, vehicle_id);
             }
+            finish();
+            Intent intent = new Intent(ReviewActivity.this, ReviewActivity.class);
+            intent.putExtra("service_id", service_id);
+            intent.putExtra("product_id", product_id);
+            intent.putExtra("store_id", store_id);
+            intent.putExtra("vehicle_id", vehicle_id);
+            startActivity(intent);
         }
 
     }
