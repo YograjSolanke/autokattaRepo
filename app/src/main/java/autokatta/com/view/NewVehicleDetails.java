0d84a3af34b5be047b136a6b9c11151bc3ec306a
ak-005 <rutunaukudkar@gmail.com>
2017-10-10 20:51:39 +0530

    10/10/2017 new vehicle details layout

---
+++ app/src/main/java/autokatta/com/view/NewVehicleDetails.java
@@ -0,0 +1,15 @@
+package autokatta.com.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import autokatta.com.R;
+
+public class NewVehicleDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_vehicle_details);
+    }
+}
