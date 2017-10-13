c986a8b9882a89214056752d3365d03d7fbba38c
ak-001 <dpkpanchal38@gmail.com>
2017-10-13 20:17:24 +0530

    13/10/2017 transfer stock response

--- app/src/main/java/autokatta/com/my_inventory_container/TransferStock.java
+++ app/src/main/java/autokatta/com/my_inventory_container/TransferStock.java
@@ -2,14 +2,17 @@ package autokatta.com.my_inventory_container;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 
 import autokatta.com.R;
 
 public class TransferStock extends AppCompatActivity {
 
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transfer_stock);
+        listView = (ListView) findViewById(R.id.transfer_list);
     }
 }
