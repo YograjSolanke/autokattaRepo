7f5aa9449e28131427c822a7edcd7abf6dfed4e6
ak-001 <dpkpanchal38@gmail.com>
2017-10-13 20:38:48 +0530

    13/10/2017 Transfer stock

--- app/src/main/java/autokatta/com/my_inventory_container/TransferStock.java
+++ app/src/main/java/autokatta/com/my_inventory_container/TransferStock.java
@@ -5,8 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
 
 import autokatta.com.R;
+import autokatta.com.apicall.ApiCall;
+import autokatta.com.interfaces.RequestNotifier;
+import autokatta.com.response.TransferListResponse;
+import retrofit2.Response;
 
-public class TransferStock extends AppCompatActivity {
+public class TransferStock extends AppCompatActivity implements RequestNotifier {
 
     ListView listView;
     @Override
@@ -14,5 +18,39 @@ public class TransferStock extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transfer_stock);
         listView = (ListView) findViewById(R.id.transfer_list);
+        getTransferData();
+    }
+
+    /*
+    Get transfer data...
+     */
+    private void getTransferData() {
+        ApiCall apiCall = new ApiCall(this, this);
+        apiCall.GetTransferVehicleNotification(getSharedPreferences(getString(R.string.my_preference), MODE_PRIVATE)
+                .getString("loginContact", ""));
+    }
+
+    @Override
+    public void notifySuccess(Response<?> response) {
+        if (response != null) {
+            if (response.isSuccessful()) {
+                TransferListResponse listResponse = (TransferListResponse) response.body();
+                if (!listResponse.getSuccess().isEmpty()) {
+                    for (TransferListResponse.Success success : listResponse.getSuccess()) {
+
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public void notifyError(Throwable error) {
+
+    }
+
+    @Override
+    public void notifyString(String str) {
+
     }
 }
