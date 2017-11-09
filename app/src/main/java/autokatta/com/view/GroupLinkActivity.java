687bc30312fb2efdea5cd52c937707826cbb9681
Yograj004 <yograj004@gmail.com>
2017-11-09 18:05:03 +0530

    09/11/17 group link activity

--- app/src/main/java/autokatta/com/view/GroupLinkActivity.java
+++ app/src/main/java/autokatta/com/view/GroupLinkActivity.java
@@ -4,10 +4,15 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import autokatta.com.R;
 
-public class GroupLinkActivity extends AppCompatActivity {
+public class GroupLinkActivity extends AppCompatActivity implements View.OnClickListener {
+    String mBundleContact;
+    int mGroupID;
+    Button mBtnJoion, mBtnCancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +22,22 @@ public class GroupLinkActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         setTitle("Group Link Requests");
 
+        mBtnJoion = (Button) findViewById(R.id.btnJoin);
+        mBtnCancel = (Button) findViewById(R.id.btnCancel);
+
+        mBtnJoion.setOnClickListener(this);
+        mBtnCancel.setOnClickListener(this);
+
+
         if (getSupportActionBar() != null) {
             getSupportActionBar().setHomeButtonEnabled(true);
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
+        if (getIntent().getExtras() != null) {
+            mGroupID = getIntent().getExtras().getInt("bundle_GroupId", 0);
+            mBundleContact = getIntent().getExtras().getString("bundle_Contact", "");
+        }
     }
 
     @Override
@@ -34,4 +50,15 @@ public class GroupLinkActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnJoin:
+
+                break;
+            case R.id.btnCancel:
+                finish();
+                break;
+        }
+    }
 }
