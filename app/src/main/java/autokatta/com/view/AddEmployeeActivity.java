b37be062c56d4c572915a93e5d451599c9215ed6
rajsanket <varude.rajashree@gmail.com>
2017-11-10 19:20:53 +0530

    10-11-17 commit(Contact fetching done in Add Employee)

--- app/src/main/java/autokatta/com/view/AddEmployeeActivity.java
+++ app/src/main/java/autokatta/com/view/AddEmployeeActivity.java
@@ -1,6 +1,12 @@
 package autokatta.com.view;
 
+import android.app.Activity;
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
@@ -10,11 +16,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import autokatta.com.R;
 import autokatta.com.apicall.ApiCall;
 import autokatta.com.interfaces.RequestNotifier;
+import autokatta.com.other.CustomToast;
 import retrofit2.Response;
+
+import static com.google.zxing.integration.android.IntentIntegrator.REQUEST_CODE;
 
 public class AddEmployeeActivity extends AppCompatActivity implements RequestNotifier, View.OnClickListener {
 
@@ -23,6 +35,9 @@ public class AddEmployeeActivity extends AppCompatActivity implements RequestNot
     EditText empName, empContact, empDescription, empDesignation;
     CheckBox permissionCheck;
     Button btnAdd;
+    ImageView imgContact;
+    TextView txtUser;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,10 +53,12 @@ public class AddEmployeeActivity extends AppCompatActivity implements RequestNot
 
         permissionCheck = (CheckBox) findViewById(R.id.permissionCheck);
         empName = (EditText) findViewById(R.id.empName);
+        imgContact = (ImageView) findViewById(R.id.contact_list);
         empContact = (EditText) findViewById(R.id.empContact);
         empDesignation = (EditText) findViewById(R.id.empDesignation);
         empDescription = (EditText) findViewById(R.id.empDescription);
         btnAdd = (Button) findViewById(R.id.addEmployee);
+        txtUser = (TextView) findViewById(R.id.txtUser);
 
         btnAdd.setOnClickListener(this);
 
@@ -66,12 +83,12 @@ public class AddEmployeeActivity extends AppCompatActivity implements RequestNot
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-
+                txtUser.setVisibility(View.GONE);
 
                 if (s.length() == 10) {
                     if (!myContact.equalsIgnoreCase(s.toString())) {
-                        // checkUser(s.toString());
-                    } else {
+                        checkUser(s.toString());
+                    } else if (empContact.equals(myContact)) {
                         empContact.requestFocus();
                         empContact.setError("admin not allowed");
                     }
@@ -80,11 +97,36 @@ public class AddEmployeeActivity extends AppCompatActivity implements RequestNot
 
             @Override
             public void afterTextChanged(Editable s) {
+                txtUser.setVisibility(View.GONE);
+            }
+        });
 
+        txtUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ActivityOptions options = ActivityOptions.makeCustomAnimation(AddEmployeeActivity.this, R.anim.left_to_right, R.anim.right_to_left);
+                Bundle bundle = new Bundle();
+                bundle.putString("contactOtherProfile", empContact.getText().toString());
+                Intent intent = new Intent(AddEmployeeActivity.this, OtherProfile.class);
+                intent.putExtras(bundle);
+                startActivity(intent, options.toBundle());
+            }
+        });
+
+        imgContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+                startActivityForResult(intent, REQUEST_CODE);
             }
         });
 
 
+    }
+
+    private void checkUser(String contact) {
+        ApiCall mApiCall = new ApiCall(this, this);
+        mApiCall.registrationContactValidation(contact);
     }
 
     @Override
@@ -114,6 +156,42 @@ public class AddEmployeeActivity extends AppCompatActivity implements RequestNot
     }
 
     @Override
+    public void onActivityResult(int reqCode, int resultCode, Intent data) {
+        super.onActivityResult(reqCode, resultCode, data);
+        switch (reqCode) {
+            case (REQUEST_CODE):
+                if (resultCode == Activity.RESULT_OK) {
+                    try {
+                        Uri contactData = data.getData();
+                        Cursor c = getContentResolver().query(contactData, null, null, null, null);
+                        if (c.moveToFirst()) {
+                            String contactId = c.getString(c.getColumnIndex(ContactsContract.Contacts._ID));
+                            String hasNumber = c.getString(c.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER));
+                            String num = "";
+                            if (Integer.valueOf(hasNumber) == 1) {
+                                Cursor numbers = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + contactId, null, null);
+                                while (numbers.moveToNext()) {
+                                    num = numbers.getString(numbers.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                                    num = num.replaceAll("-", "");
+                                    num = num.replace("(", "").replace(")", "").replaceAll(" ", "").replaceAll("[\\D]", "");
+
+                                    if (num.length() > 10)
+                                        num = num.substring(num.length() - 10);
+                                    Toast.makeText(AddEmployeeActivity.this, "Number=" + num, Toast.LENGTH_LONG).show();
+                                    empContact.setText(num);
+                                    checkUser(num);
+                                }
+                            }
+                        }
+                        break;
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+        }
+    }
+
+    @Override
     public void notifySuccess(Response<?> response) {
 
     }
@@ -125,6 +203,17 @@ public class AddEmployeeActivity extends AppCompatActivity implements RequestNot
 
     @Override
     public void notifyString(String str) {
+        if (str != null) {
+            if (str.equalsIgnoreCase("Success")) {
+                txtUser.setVisibility(View.VISIBLE);
+                //txtInvite.setVisibility(View.GONE);
+
+            } else {
+                empContact.setError("Not Autokatta User");
+                txtUser.setVisibility(View.GONE);
+            }
+        } else
+            CustomToast.customToast(getApplicationContext(), getString(R.string.no_response));
 
     }
 
