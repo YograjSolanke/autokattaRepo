6e5145edb528ac088879ababb71646122a4a0553
Yograj004 <yograj004@gmail.com>
2017-11-29 18:34:02 +0530

    29-11-17 suggestion for profile commit

--- app/src/main/java/autokatta/com/adapter/SuggestionProfileAdapter.java
+++ app/src/main/java/autokatta/com/adapter/SuggestionProfileAdapter.java
@@ -20,7 +20,7 @@ import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import java.util.List;
 
 import autokatta.com.R;
-import autokatta.com.response.ModelSuggestionsResponse;
+import autokatta.com.response.SuggestionsResponse;
 import autokatta.com.view.OtherProfile;
 import autokatta.com.view.UserProfile;
 
@@ -31,16 +31,17 @@ import autokatta.com.view.UserProfile;
 public class SuggestionProfileAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     Activity mActivity;
-    private List<ModelSuggestionsResponse> mSuggestionList;
+    private List<SuggestionsResponse.Success.Profile> mSuggestionList;
     private TextView mSuggestionAbout;
     private String mLoginContact;
 
     /*Constructor*/
-    public SuggestionProfileAdapter(Activity mActivity1, List<ModelSuggestionsResponse> storeResponseArrayList1, TextView txtSuggestion, String mLoginContact) {
+    public SuggestionProfileAdapter(Activity mActivity1, List<SuggestionsResponse.Success.Profile> storeResponseArrayList1, TextView txtSuggestion, String mLoginContact) {
         mActivity = mActivity1;
         mSuggestionList = storeResponseArrayList1;
         mSuggestionAbout = txtSuggestion;
         this.mLoginContact = mLoginContact;
+        setHasStableIds(true);
     }
 
     @Override
@@ -63,7 +64,7 @@ public class SuggestionProfileAdapter extends RecyclerView.Adapter<RecyclerView.
         CardView mProfileCardView;
         ImageView mProfilePic;
         Button mView;
-        TextView mUserName;
+        TextView mUserName, mLocation;
 
         private ProfileSuggestions(View profileView) {
             super(profileView);
@@ -71,32 +72,31 @@ public class SuggestionProfileAdapter extends RecyclerView.Adapter<RecyclerView.
             mProfilePic = (ImageView) profileView.findViewById(R.id.profilePic);
             mView = (Button) profileView.findViewById(R.id.btnView);
             mUserName = (TextView) profileView.findViewById(R.id.userName);
+            mLocation = (TextView) profileView.findViewById(R.id.userLocation);
         }
     }
-
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         Log.i("viewType", "Suggestion->" + viewType);
         View mView;
-
         mView = LayoutInflater.from(parent.getContext()).inflate(R.layout.suggestion_profile_layout_adapter, parent, false);
         return new ProfileSuggestions(mView);
-
-
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
 
         final ProfileSuggestions mProfileSuggestions = (ProfileSuggestions) holder;
+        mProfileSuggestions.setIsRecyclable(false);
 
-        mSuggestionAbout.setText("Suggestion Based On Skills");
-        mProfileSuggestions.mUserName.setText(mSuggestionList.get(position).getName());
-        if (mSuggestionList.get(position).getImage().equals("")) {
+        mSuggestionAbout.setText("Suggestion Based On Profile");
+        mProfileSuggestions.mUserName.setText(mSuggestionList.get(position).getUserName());
+        mProfileSuggestions.mLocation.setText(mSuggestionList.get(position).getCity());
+        if (mSuggestionList.get(position).getProfilePicture().equals("")) {
 
             Glide.with(mActivity)
-                    .load(mActivity.getString(R.string.base_image_url) + mSuggestionList.get(position).getImage())
+                    .load(mActivity.getString(R.string.base_image_url) + mSuggestionList.get(position).getProfilePicture())
                     // .bitmapTransform(new CropSquareTransformation(mActivity))
                     .diskCacheStrategy(DiskCacheStrategy.ALL)
                     .into(mProfileSuggestions.mProfilePic);
@@ -108,26 +108,20 @@ public class SuggestionProfileAdapter extends RecyclerView.Adapter<RecyclerView.
             @Override
             public void onClick(View v) {
                 Bundle bundle = new Bundle();
-                bundle.putString("contactOtherProfile", mSuggestionList.get(mProfileSuggestions.getAdapterPosition()).getUserContact());
-                if (mLoginContact.equalsIgnoreCase(mSuggestionList.get(mProfileSuggestions.getAdapterPosition()).getUserContact())) {
+                bundle.putString("contactOtherProfile", mSuggestionList.get(mProfileSuggestions.getAdapterPosition()).getContactNo());
+                if (mLoginContact.equalsIgnoreCase(mSuggestionList.get(mProfileSuggestions.getAdapterPosition()).getContactNo())) {
                     ActivityOptions options = ActivityOptions.makeCustomAnimation(mActivity, R.anim.ok_left_to_right, R.anim.ok_right_to_left);
                     Intent i = new Intent(mActivity, UserProfile.class);
                     i.putExtras(bundle);
                     mActivity.startActivity(i, options.toBundle());
-
                 } else {
                     ActivityOptions options = ActivityOptions.makeCustomAnimation(mActivity, R.anim.ok_left_to_right, R.anim.ok_right_to_left);
                     Intent i = new Intent(mActivity, OtherProfile.class);
                     i.putExtras(bundle);
                     mActivity.startActivity(i, options.toBundle());
-
                 }
             }
         });
-
-
     }
-
-
 }
 
